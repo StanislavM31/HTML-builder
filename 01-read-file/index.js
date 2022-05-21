245a4bf70e738b86daa230cc6d8718ac9b6268a5
Stanislav Minko <minkostas@gmail.com>
2022-05-21 16:55:51 +0300

    fix: 01-read-file(null check)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,9 @@
 /* const { stdout } = process;
 stdout.write('Node.js'); */
 let fs = require('fs');
-let fileContent = fs.readFileSync('text.txt', 'utf8');
-console.log(fileContent);
-/* const { stdin, stdout } = process;
-stdin.on('data', data => stdout.write(data));
-process.exit(); */
+
+let stream = new fs.ReadStream('text.txt', 'utf8');
+stream.on('readable', function(){
+  let data = stream.read();
+  if(data != null)console.log(data);
+});
