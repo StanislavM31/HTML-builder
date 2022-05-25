bf978fcf475aec92bf04c3b548ca9595cc406d1c
Stanislav Minko <minkostas@gmail.com>
2022-05-25 17:04:05 +0300

    fix: 01-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,9 +2,7 @@
 /* const { stdout } = process;
 stdout.write('Node.js'); */
 let fs = require('fs');
-
-let stream = new fs.ReadStream('text.txt', 'utf8');
-stream.on('readable', function(){
-  let data = stream.read();
-  if(data != null)console.log(data);
-});
+const path = require('path');
+const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+input.on('data', data => console.log(data));
+input.on('error', error => console.log('Error', error.message));
